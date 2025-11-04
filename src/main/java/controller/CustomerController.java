30dc0355c0f9e8e9346264dc8eeb997a8c3e57f5
itzisurii <itzIsurika08@gmail.com>
2025-11-04 11:03:23 +0530

    Created customerFXML, CustomerController and CustomerDTO

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -1,4 +1,93 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colDOB;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colProvince;
+
+    @FXML
+    private TableColumn<?, ?> colSalary;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableView<?> tblCustomerManagement;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCusID;
+
+    @FXML
+    private TextField txtDOB;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtProvince;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnAddActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackToDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteActionOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/model/dto/CustomerDTO.java
+++ src/main/java/model/dto/CustomerDTO.java
@@ -1,4 +1,20 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CustomerDTO {
+    private String id;
+    private String title;
+    private String name;
+    private String dob;
+    private double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
 }
